d1a28dccd64b02e87cb85e7d4d4ce07104fd79fc
Rifat Kudryakov <flungloaf@gmail.com>
2020-07-08 08:58:14 +0300

    Add detecting if the image didn't change after normalization

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import jimp from "jimp";
 import Discord from "discord.js";
 import "dotenv/config";
+import looksSame from  "looks-same";
 
 const client = new Discord.Client();
 
@@ -15,10 +16,19 @@ client.on("message", message => {
       if (a.name && a.name.match(imageExt)) {
         const image = await jimp.read(a.url);
         const mime = image.getMIME();
+        const original = await image.getBufferAsync(mime);
         image.normalize();
         const buffer = await image.getBufferAsync(mime);
-        const attachment = new Discord.MessageAttachment(buffer);
-        message.channel.send(attachment);
+        looksSame(original, buffer, (err, res) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          if (!res.equal) {
+            const attachment = new Discord.MessageAttachment(buffer);
+            message.channel.send(attachment);
+          }
+        })
       }
     });
   }
